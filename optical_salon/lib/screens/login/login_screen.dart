3522acb9210f75e100846621f8a890df4983d3f0
Vladislav Taraikovich <54981764+soulsodev@users.noreply.github.com>
2021-04-26 20:51:41 +0300

    add: login and registration screens UI.

---
+++ optical_salon/lib/screens/login/login_screen.dart
@@ -0,0 +1,125 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:optical_salon/screens/registration/registration_screen.dart';
+
+class LoginScreen extends StatefulWidget {
+  static String tag = 'login-screen';
+
+  @override
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final signInButton = Padding(
+      padding: EdgeInsets.only(top: 16.0),
+      // ignore: deprecated_member_use
+      child: ElevatedButton(
+        child: Text("Sign In"),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: Colors.lightBlueAccent,
+          onPrimary: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+        ),
+      ),
+    );
+
+    final signUpButton = OutlinedButton(
+        child: Text('Sign Up'),
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => RegistrationScreen(),
+          ),
+        ),
+        style: ElevatedButton.styleFrom(
+          side: BorderSide(width: 2.0, color: Colors.lightBlueAccent),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+        ),
+      );
+
+    final forgotLabel = TextButton(
+      child: Text(
+        'Forgot password?',
+        style: TextStyle(
+          color: Colors.black54,
+        ),
+      ),
+      onPressed: () {},
+    );
+
+    final formLabel = Text(
+      'Login',
+      style: TextStyle(
+        color: Colors.black54,
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
+            Align(
+              alignment: Alignment.center,
+              child: formLabel,
+            ),
+            SizedBox(height: 16.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 8.0),
+            signInButton,
+            signUpButton,
+            forgotLabel
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ optical_salon/lib/screens/registration/registration_screen.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:optical_salon/screens/login/login_screen.dart';
+
+class RegistrationScreen extends StatefulWidget {
+  static String tag = 'registration-screen';
+
+  @override
+  _RegistrationScreenState createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final logo = Hero(
+      tag: 'hero',
+      child: CircleAvatar(
+        backgroundColor: Colors.transparent,
+        radius: 48.0,
+        child: Image.asset('assets/images/logo.png'),
+      ),
+    );
+
+    final name = TextFormField(
+      keyboardType: TextInputType.name,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Name',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final phone = TextFormField(
+      keyboardType: TextInputType.phone,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: '+375 (XX) XXX-XX-XX',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final email = TextFormField(
+      keyboardType: TextInputType.emailAddress,
+      autofocus: false,
+      decoration: InputDecoration(
+        hintText: 'Email',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final password = TextFormField(
+      autofocus: false,
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: 'Password',
+        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(32.0),
+        ),
+      ),
+    );
+
+    final signUpButton = Padding(
+      padding: EdgeInsets.only(top: 16.0),
+      // ignore: deprecated_member_use
+      child: ElevatedButton(
+        child: Text("Sign Up"),
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          primary: Colors.lightBlueAccent,
+          onPrimary: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+        ),
+      ),
+    );
+
+    final signInButton = Padding(
+      padding: EdgeInsets.only(bottom: 8.0),
+      // ignore: deprecated_member_use
+      child: OutlinedButton(
+        child: Text('Sign In'),
+        onPressed: () => Navigator.pop(context),
+        style: ElevatedButton.styleFrom(
+          side: BorderSide(width: 2.0, color: Colors.lightBlueAccent),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(24.0),
+          ),
+        ),
+      ),
+    );
+
+    final formLabel = Text(
+      'Registration',
+      style: TextStyle(
+        color: Colors.black54,
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: ListView(
+          shrinkWrap: true,
+          padding: EdgeInsets.only(left: 24.0, right: 24.0),
+          children: <Widget>[
+            logo,
+            SizedBox(height: 48.0),
+            Align(
+              alignment: Alignment.center,
+              child: formLabel,
+            ),
+            SizedBox(height: 16.0),
+            name,
+            SizedBox(height: 8.0),
+            phone,
+            SizedBox(height: 8.0),
+            email,
+            SizedBox(height: 8.0),
+            password,
+            SizedBox(height: 8.0),
+            signUpButton,
+            signInButton
+          ],
+        ),
+      ),
+    );
+  }
+}
